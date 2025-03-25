40698da90849e7c5f7a029dd0889bfdd8b7438c4
BadHenry7 <141955809+BadHenry7@users.noreply.github.com>
2025-03-25 11:25:58 -0500

    Se trabajo en historias y telegram

--- actions/actions.py
+++ actions/actions.py
@@ -32,7 +32,35 @@ class BuscarCitas(Action):
 
     def name(self) -> Text:
         return "action_ultima_cita"
-    
+
+    async def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        user_cedula = tracker.get_slot("cedula")
+        print("--------------", user_cedula)
+        
+        payload = {
+            "cedula": user_cedula 
+        }
+        async with aiohttp.ClientSession() as session:
+            async with session.post("http://127.0.0.1:8000/get_ultima_cita", json=payload) as response:
+                rasa_response = await response.json()
+                print(rasa_response)
+                v_fecha = rasa_response["fecha"]
+                v_doctor = rasa_response["doctor"]
+
+                dispatcher.utter_message(
+                response="utter_ultima_cita",
+                fecha=v_fecha,
+                doctor=v_doctor
+        )
+        return []
+
+
+class Buscar(Action):
+    def name(self) -> Text:
+        return "action_"
 
     async def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
