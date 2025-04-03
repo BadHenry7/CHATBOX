4aac358aff792853da4202b3d66f4daa176b05a2
BadHenry7 <141955809+BadHenry7@users.noreply.github.com>
2025-04-03 12:57:36 -0500

    intentando poner 4 action mas

--- actions/actions.py
+++ actions/actions.py
@@ -44,6 +44,10 @@ class BuscarCitas(Action):
         payload = {
             "cedula": user_cedula 
         }
+        if not user_cedula:
+            dispatcher.utter_message(response="utter_dar_cedula")  
+            return []  # Indica que el slot sigue vacío
+
         async with aiohttp.ClientSession() as session:
             async with session.post("http://127.0.0.1:8000/get_ultima_cita", json=payload) as response:
                 rasa_response = await response.json()
@@ -59,31 +63,134 @@ class BuscarCitas(Action):
         return []
 
 
-# class Buscar(Action):
-#     def name(self) -> Text:
-#         return "action_"
+class Diagnosticos(Action):
 
-#     async def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+    def name(self) -> Text:
+        return "action_diagnosticos_chatbox"
 
-#         user_cedula = tracker.get_slot("cedula")
-#         print("--------------", user_cedula)
+    async def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        user_cedula = tracker.get_slot("cedula")
+        print("--------------", user_cedula)
         
-#         payload = {
-#             "cedula": user_cedula 
-#         }
-#         async with aiohttp.ClientSession() as session:
-#             async with session.post("http://127.0.0.1:8000/get_ultima_cita", json=payload) as response:
-#                 rasa_response = await response.json()
-#                 print(rasa_response)
-#                 v_fecha = rasa_response["fecha"]
-#                 v_doctor = rasa_response["doctor"]
-     
+        payload = {
+            "cedula": user_cedula 
+        }
 
-#                 dispatcher.utter_message(
-#                 response="utter_ultima_cita",
-#                 fecha=v_fecha,
-#                 doctor=v_doctor
-#         )
-#         return []
+        if not user_cedula:
+            dispatcher.utter_message(response="utter_dar_cedula")  
+            return []  # Indica que el slot sigue vacío
+
+        async with aiohttp.ClientSession() as session:
+            async with session.post("http://127.0.0.1:8000/get_diagnosticos_chatbox", json=payload) as response:
+                rasa_response = await response.json()
+                print(rasa_response)
+                v_resultado = rasa_response["resultado"]
+                v_descripcion = rasa_response["descripcion"]
+
+                dispatcher.utter_message(
+                response="utter_diagnosticos_chatbox",
+                resultado=v_resultado,
+                descripcion=v_descripcion
+        )
+        return []
+    
+
+class Recomendaciones(Action):
+
+    def name(self) -> Text:
+        return "action_recomendaciones_chatbox"
+
+    async def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        user_cedula = tracker.get_slot("cedula")
+        print("--------------", user_cedula)
+        
+        payload = {
+            "cedula": user_cedula 
+        }
+        if not user_cedula:
+            dispatcher.utter_message(response="utter_dar_cedula")  
+            return []  # 
+        
+        async with aiohttp.ClientSession() as session:
+            async with session.post("http://127.0.0.1:8000/get_recomendaciones_chatbox", json=payload) as response:
+                rasa_response = await response.json()
+                print(rasa_response)
+                v_observacion = rasa_response["observacion"]
+              
+
+                dispatcher.utter_message(
+                response="utter_recomendaciones_chatbox",
+                observacion=v_observacion,
+            
+        )
+        return []
+    
+class Sintomas(Action):
+
+    def name(self) -> Text:
+        return "action_sintomas_chatbox"
+
+    async def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        user_cedula = tracker.get_slot("cedula")
+        print("--------------", user_cedula)
+        
+        payload = {
+            "cedula": user_cedula 
+        }
+        if not user_cedula:
+            dispatcher.utter_message(response="utter_dar_cedula")  
+            return []  
+        async with aiohttp.ClientSession() as session:
+            async with session.post("http://127.0.0.1:8000/get_sintomas_chatbox", json=payload) as response:
+                rasa_response = await response.json()
+                print(rasa_response)
+                v_nombre = rasa_response["nombre"]
+                v_descripcion = rasa_response["descripcion"]
+
+                dispatcher.utter_message(
+                response="utter_sintomas_chatbox",
+                nombre=v_nombre,
+                descripcion=v_descripcion
+        )
+        return []
+    
+class Ubicacion(Action):
+
+    def name(self) -> Text:
+        return "action_ubicacion_chatbox"
+
+    async def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        user_cedula = tracker.get_slot("cedula")
+        print("--------------", user_cedula)
+        
+        payload = {
+            "cedula": user_cedula 
+        }
+        if not user_cedula:
+            dispatcher.utter_message(response="utter_dar_cedula")  
+            return [] 
+        async with aiohttp.ClientSession() as session:
+            async with session.post("http://127.0.0.1:8000/get_ubicacion_chatbox", json=payload) as response:
+                rasa_response = await response.json()
+                print(rasa_response)
+                v_ubicacion = rasa_response["ubicacion"]
+                v_salas = rasa_response["salas"]
+
+                dispatcher.utter_message(
+                response="utter_ubicacion_chatbox",
+                ubicacion=v_ubicacion,
+                salas=v_salas
+        )
+        return []
